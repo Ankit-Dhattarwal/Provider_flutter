5b90c29766344a5ab166dcae76def7c5c172eda3
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2024-05-16 15:37:23 +0530

    Provider part 2 use ChnageNotifier Provider

---
+++ lib/Provider2/providerChangeNotifier.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Provider Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Basic Provider'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<MyModel>(
+      create: (BuildContext context) => MyModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Container(
+                child: Consumer<MyModel>(builder: (context, MyModel, child) {
+                  return Text('${MyModel.someValue}');
+                }),
+              ),
+              Container(
+                child: Consumer<MyModel>(builder: (context, MyModel, child) {
+                  return ElevatedButton(
+                      onPressed: () {
+                        MyModel.doSomething();
+                      }, child: const Text('Change Value'));
+                }),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class MyModel with ChangeNotifier {
+  int someValue = 0;
+  void doSomething() {
+    someValue++;
+    notifyListeners();
+    print(someValue);
+  }
+}
